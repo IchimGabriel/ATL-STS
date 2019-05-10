1d3dc45da7df565f5c2209492586b58069a859e9
Gabriel Ichim <ichimgabriel75@gmail.com>
2019-05-10 02:55:39 +0100

    ATL - Security Token Service
    Identity Server

---
+++ SeedData.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using System;
+using System.Linq;
+using System.Security.Claims;
+using IdentityModel;
+using ATL_STS.Data;
+using ATL_STS.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ATL_STS
+{
+    public class SeedData
+    {
+        public static void EnsureSeedData(string connectionString)
+        {
+            var services = new ServiceCollection();
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(connectionString));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                using (var scope = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    var context = scope.ServiceProvider.GetService<ApplicationDbContext>();
+                    context.Database.Migrate();
+
+                    var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+                    var gabriel = userMgr.FindByNameAsync("gabriel").Result;
+                    if (gabriel == null)
+                    {
+                        gabriel = new ApplicationUser
+                        {
+                            UserName = "gabriel"
+                        };
+                        var result = userMgr.CreateAsync(gabriel, "Vrabie75##").Result;
+                        if (!result.Succeeded)
+                        {
+                            throw new Exception(result.Errors.First().Description);
+                        }
+
+                        result = userMgr.AddClaimsAsync(gabriel, new Claim[]{
+                        new Claim(JwtClaimTypes.Name, "Gabriel Ichim"),
+                        new Claim(JwtClaimTypes.GivenName, "Gabriel"),
+                        new Claim(JwtClaimTypes.FamilyName, "Ichim"),
+                        new Claim(JwtClaimTypes.Email, "GabrielIchim@email.com"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "http://gabriel.com"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 69118, 'country': 'Germany' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }).Result;
+                        if (!result.Succeeded)
+                        {
+                            throw new Exception(result.Errors.First().Description);
+                        }
+                        Console.WriteLine("gabriel created");
+                    }
+                    else
+                    {
+                        Console.WriteLine("gabriel already exists");
+                    }
+                }
+            }
+        }
+    }
+}
